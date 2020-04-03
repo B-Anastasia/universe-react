dfffea318f93734a26a2caaaa34f736081f586c8
Anastasia Boshniak <boshniak.a@gmail.com>
2020-04-03 12:48:18 +0300

    Added people list from API

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
 import './item-list.css';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner";
 
-const ItemList = () =>{
-    return(
-        <ul className="item-list list-group left-side-list" >
-            <li className='list-group-item'>Name1</li>
-            <li className='list-group-item'>Name2</li>
-            <li className='list-group-item'>Name3</li>
-        </ul>
-    );
+export default class ItemList extends Component{
+
+    swapiService = new SwapiService();
+
+    state={
+        peopleList : null,
+    };
+
+    componentDidMount(){
+        this.swapiService
+            .getAllPeople()
+            .then ( (peopleList)=>{
+                this.setState({
+                    peopleList
+                });
+            });
+    }
+
+    renderItems (arr){
+        return arr.map(({id, name})=>{
+            return (
+                <li
+                    className='list-group-item'
+                    id={id}
+                    onClick={()=>{this.props.onItemSelected(id)}}>
+                    {name}
+                </li>
+            );
+        })
+    }
+
+    render(){
+        const { peopleList} = this.state;
+        if(!peopleList){
+            return <Spinner/>
+        }
+        const list = this.renderItems(peopleList);
+        return(
+            <ul className="item-list list-group left-side-list" >
+                {list}
+            </ul>
+        );
+    }
+
 };
-
-export default ItemList;
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -44,7 +44,7 @@ export default class SwapiService {
         return item.url.match(idRegExp)[1];
     }
 
-    _transformPlanet(planet){
+    _transformPlanet=(planet)=>{
 
         return{
             id: this._extractId(planet),
@@ -53,9 +53,9 @@ export default class SwapiService {
             rotationPeriod: planet.rotation_perid,
             diameter: planet.diameter
         }
-    }
+    };
 
-    _transformPerson(person){
+    _transformPerson=(person)=>{
         return {
             id: this._extractId(person),
             name: person.name,
@@ -64,9 +64,9 @@ export default class SwapiService {
             eyeColor: person.eye_color,
             height: person.heigth
         }
-    }
+    };
 
-    _transformStarship(starship){
+    _transformStarship=(starship)=>{
         return{
             id: this._extractId(starship),
             name: starship.name,
